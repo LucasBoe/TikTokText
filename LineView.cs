84053d9c01dfae964f41fa3d02c2b183e36c5d2b
LucasBoe <lucas@lumotex.de>
2024-06-12 20:02:03 +0200

    added line and text animation handling

---
+++ LineView.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+    public class LineView : MonoBehaviour
+    {
+        [SerializeField] CanvasGroup canvasGroup;
+        [SerializeField] TMP_Text textDummy;
+
+        Queue<TMP_Text> words = new();
+
+        internal void Init(string line)
+        {
+            foreach (var word in line.Split(' '))
+            {
+                var instance = Instantiate(textDummy, textDummy.transform.parent);
+                instance.text = word;
+                instance.transform.localScale = Vector3.zero;
+                words.Enqueue(instance);
+            }
+
+            canvasGroup.alpha = 0;
+            textDummy.gameObject.SetActive(false);
+        }
+
+        internal bool TryAnimate()
+        {
+            if (words.Count <= 0)
+            {
+                var _out = DOTween.Sequence();
+
+                _out.Append(canvasGroup.transform.DOLocalMoveY(-500, .1f).SetEase(Ease.InBack));
+                _out.AppendCallback(() => canvasGroup.alpha = 0);
+                return false;
+            }
+
+            var active = words.Dequeue();
+
+            active.transform.rotation = Quaternion.Euler(0,0,45f);
+
+            active.transform.DORotate(new Vector3(0,0,4), .3f).SetEase(Ease.OutBack);
+            active.transform.DOScaleX(1.2f, .2f).SetEase(Ease.OutBack);
+            active.transform.DOScaleY(1.2f, .3f).SetEase(Ease.OutBounce);
+
+            active.transform.DOScale(1, .25f).SetEase(Ease.OutSine).SetDelay(.5f);
+            active.transform.DORotate(Vector3.zero, .15f).SetEase(Ease.InOutSine).SetDelay(.5f);
+
+            canvasGroup.alpha = 1;
+            return true;
+        }
+    }
---
+++ TextAnimator.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+    public class TextAnimator : MonoBehaviour
+    {
+        [SerializeField] LineView lineDummy;
+        [SerializeField, TextArea] string text;
+
+        private bool initiated = false;
+        private bool show = false;
+
+        private void Start()
+        {
+            lineDummy.gameObject.SetActive(false);
+        }
+        [Button] private void Test()
+        {
+            Animate(text);
+        }
+        public void Animate(string text)
+        {
+            if (initiated)
+                return;
+
+            initiated = true;
+
+            Queue<LineView> lines = new();
+
+            foreach (var line in Split(text))
+            {
+                var instance = Instantiate(lineDummy, lineDummy.transform.parent);
+                instance.gameObject.SetActive(true);
+                instance.Init(line);
+                lines.Enqueue(instance);
+            }
+
+            StartCoroutine(AnimationRoutine(lines));
+        }
+
+        private static string[] Split(string text)
+        {
+            return text.Split('\n', '.');
+        }
+
+        private IEnumerator AnimationRoutine(Queue<LineView> lines)
+        {
+            while (lines.Count > 0)
+            {
+                var activeLine = lines.Dequeue();
+
+                while (activeLine.TryAnimate())
+                {
+                    while (!show)
+                        yield return null;
+                    
+                    show = false;
+                }
+            }
+        }
+        private void OnGUI()
+        {
+            if (GUILayout.Button("Next\nnn"))
+            {
+                show = true;
+
+
+                Test();
+            }
+        }
+    }
